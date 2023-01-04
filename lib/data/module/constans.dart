352e3ebd11686e6071a714b930c9c90ef95d7b29
zn-gostaran <zn.gostaran@gmail.com>
2023-01-04 04:35:01 +0330

    fix code

--- lib/data/module/constans.dart
+++ lib/data/module/constans.dart
@@ -5,14 +5,7 @@ class ColorSelecet {
   static Color surface = const Color.fromARGB(255, 218, 230, 246);
 
   static Color hover = const Color(0xFF142952);
-  static Color bgFade = const Color(0xFFF6FAFD);
-  static Color line = const Color(0xFFB8BEC3);
-  static Color searchBox = const Color(0xFFE8F1F1);
-  static Color folder = const Color(0xFFFFC855);
-  static Color indicator = const Color(0xFFC9E0FF);
   static Color black = const Color.fromARGB(255, 0, 0, 0);
-  static Color pink = const Color(0xFFFF65D4);
-
   static Color primery = const Color(0xFF006AE9);
   static Color bg = const Color(0xFFFCFCFC);
   static Color text = const Color.fromARGB(255, 0, 0, 0);
@@ -21,41 +14,4 @@ class ColorSelecet {
   static Color unSelectColortab = const Color(0xFFA5A3A9);
   static Color green = const Color(0xFF57AE24);
   static Color grey = const Color(0xFFD2D1D4);
-}
-
-MaterialColor generateMaterialColor(Color color) {
-  return MaterialColor(color.value, {
-    50: tintColor(color, 0.9),
-    100: tintColor(color, 0.8),
-    200: tintColor(color, 0.6),
-    300: tintColor(color, 0.4),
-    400: tintColor(color, 0.2),
-    500: color,
-    600: shadeColor(color, 0.1),
-    700: shadeColor(color, 0.2),
-    800: shadeColor(color, 0.3),
-    900: shadeColor(color, 0.4),
-  });
-}
-
-int tintValue(int value, double factor) =>
-    max(0, min((value + ((255 - value) * factor)).round(), 255));
-
-Color tintColor(Color color, double factor) => Color.fromRGBO(
-    tintValue(color.red, factor),
-    tintValue(color.green, factor),
-    tintValue(color.blue, factor),
-    1);
-
-int shadeValue(int value, double factor) =>
-    max(0, min(value - (value * factor).round(), 255));
-
-Color shadeColor(Color color, double factor) => Color.fromRGBO(
-    shadeValue(color.red, factor),
-    shadeValue(color.green, factor),
-    shadeValue(color.blue, factor),
-    1);
-
-class Palette {
-  static const Color primary = Color(0xFF1F5FD9);
 }
